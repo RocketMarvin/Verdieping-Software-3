41d2138e26803fe9b996451257cf8d60893e686b
Marvin Jonker <mjonker13@st.noorderpoort.nl>
2025-01-07 21:57:30 +0100

    hand physics

---
+++ VS3 UnityXR/Assets/Scripts/HandPresencePhysics.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class HandPresencePhysics : MonoBehaviour
+{
+
+    public Transform target;
+    private Rigidbody rb;
+    [SerializeField] private Collider[] handColliders;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        handColliders = GetComponentsInChildren<Collider>();
+    }
+
+    public void EnableHandCollider()
+    {
+        foreach (var item in handColliders)
+        {
+            item.enabled = true;
+        }
+    }
+
+    public void EnableHandColliderDelay(float delay)
+    {
+        Invoke("EnableHandCollider", delay);
+    }
+    public void DisableHandCollider()
+    {
+        foreach (var item in handColliders)
+        {
+            item.enabled = false;
+            print("tetst");
+        }
+    }
+}
